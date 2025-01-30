43a35873bca883577a48b55bc41c17d0fdf97611
AndreaMargutti <marguttiandrea30@gmail.com>
2025-01-30 16:16:47 +0100

    started with api call

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
-import axios from "axios";
+const apiUrl = 'https://api.github.com/users';
+
+const inputForm = document.getElementById('search-form');
+
+
+inputForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const inputUsername = document.getElementById('search-bar').value;
+    console.log(inputUsername.replaceAll(' ', ''));
+
+    // esegui la chiamata all'endpoint
+})
