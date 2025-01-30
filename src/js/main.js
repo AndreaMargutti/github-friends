5dad5175fe62131822d33dfc663a575afbd04de0
AndreaMargutti <marguttiandrea30@gmail.com>
2025-01-30 16:43:37 +0100

    completed api call

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const apiUrl = 'https://api.github.com/users';
 
 const inputForm = document.getElementById('search-form');
@@ -6,8 +8,16 @@ const inputForm = document.getElementById('search-form');
 inputForm.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    const inputUsername = document.getElementById('search-bar').value;
-    console.log(inputUsername.replaceAll(' ', ''));
+    let inputUsername = document.getElementById('search-bar').value;
+    // console.log(inputUsername.replaceAll(' ', ''));
+    inputUsername = inputUsername.replaceAll(' ', '');
 
     // esegui la chiamata all'endpoint
+    axios.get(`${apiUrl}/${inputUsername}`)
+        .then(function (response) {
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 })
