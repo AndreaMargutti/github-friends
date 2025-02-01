09ed5fed596cdb4d2495c9fe8ed243eb1f67ad6d
AndreaMargutti <marguttiandrea30@gmail.com>
2025-02-01 13:01:12 +0100

    add remove of html

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,9 @@ const resultContainer = document.getElementById('result');
 
 // funzione per renderizzare i dati dell'utente
 function renderUserData(userData) {
+    // svuoto tutta la sezione
+    resultContainer.innerHTML = '';
+
     // rendo visibile la sezione
     resultContainer.classList.remove('d-none');
 
@@ -15,14 +18,15 @@ function renderUserData(userData) {
     const userName = document.createElement('h1');
     userName.textContent = userData.login;
     let userPic = document.createElement('img');
+    userPic.classList.add('pt-2');
     userPic.src = userData.avatar_url;
 
     //creo nodi di testo per location e repo
     const userLocation = document.createElement('h2');
     userLocation.classList.add('pt-2');
-    userLocation.textContent = userData.location;
+    userLocation.textContent = `Location: ${userData.location}`;
     const userRepos = document.createElement('h3');
-    userRepos.textContent = `Repository Pubbliche: ${userData.public_repos}`;
+    userRepos.textContent = `Public Repository: ${userData.public_repos}`;
 
 
     //inserisco avatar e nome nel figure
